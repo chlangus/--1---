29acecfadb695bf8e40543accf8f00b13d4ee1a0
chlangus <angusangus11@gmail.com>
2024-01-25 17:29:41 +0900

    💄 : audio 버튼 및 reaction 버튼 UI 변경

--- src/services/storeId.jsx
+++ src/services/storeId.jsx
@@ -1,15 +1,23 @@
 export default async function storeId(name) {
-  const response = await fetch(
-    'https://openmind-api.vercel.app/3-2/subjects/',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        name,
-      }),
-    },
-  );
+  try {
+    const response = await fetch(
+      'https://openmind-api.vercel.app/3-2/subjects/',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name,
+        }),
+      },
+    );
 
-  const result = await response.json();
-  return result;
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    return null;
+  }
 }
