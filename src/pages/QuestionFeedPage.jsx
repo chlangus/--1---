b901edb82d0b86b0f5b2795f282f086a6a905f52
Yoojungchoii <geniusyj@gmail.com>
2024-01-28 00:58:28 +0900

    🚧 : 무한스크롤 데이터가 중복됐다가 아니었다가 함

--- src/pages/QuestionFeedPage.jsx
+++ src/pages/QuestionFeedPage.jsx
@@ -15,15 +15,14 @@ export default function QuestionFeedPage() {
   const { id } = useParams();
   const [subjectId, setSubjectId] = useState(id);
   const [questions, setQuestions] = useState([]);
+  const [subjectData, setSubjectData] = useSubjectDataRecoil();
   const [ref, inView] = useInView({
     triggerOnce: false,
     rootMargin: '100px',
   });
-  // const [moreData, setMoreData] = useState([]);
-
-  const [subjectData, setSubjectData] = useSubjectDataRecoil();
-
-  console.log(subjectData);
+  const [offset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(false);
+  let limit = 8;
 
   useEffect(() => {
     fetchQuestion(subjectId).then(data => {
@@ -48,33 +47,47 @@ export default function QuestionFeedPage() {
     });
   }, []);
 
+  // ============무한스크롤 구현 시도중====================================
+  async function getQuestions() {
+    const query = `limit=${limit}&offset=${offset}`; //
+    console.log(query);
+    const response = await fetch(
+      `https://openmind-api.vercel.app/3-2/subjects/${subjectId}/questions/?${query}`,
+    );
+    const result = await response.json();
+    if (result.results.length < limit) limit = result.results.length;
+    console.log(result);
+    return result;
+  }
+  // 안녕하십니까!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 어 그런가요? 16개만 나오는듯..!?
+  // 오 어떤가요 먼가 달라졌죠
   const loadMoreQuestions = async () => {
+    // 좋은 접근입니다!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     try {
-      const data = await fetchQuestion(subjectId, offset, limit);
+      const data = await getQuestions();
       const transformedQuestions = data.results.map(question => ({
         ...question,
       }));
-
-      setQuestions(prevData => [...prevData, ...transformedQuestions]);
-
-      console.log('data:', data);
+      console.log('transformedData: ', transformedQuestions);
+      if (data.results.length) {
+        // 만약 결과가 있다면 상태를 업데이트 ㅎ 됐당!!!!!!!!!!!!!!!!!!
+        setOffset(prevOffset => prevOffset + limit);
+        setQuestions(prevData => [...prevData, ...transformedQuestions]);
+      }
+      if (loading) return; // 이미 로딩 중이면 중복 호출 방지
+      setLoading(true);
     } catch (error) {
       console.error('데이터를 가져오는 중 오류 발생:', error);
     }
   };
+  // 라이브러리를 안 쓰는 것이 덜 복잡할 수도 있습니다!
 
-  // 맨 처음 렌더링 되었을 때 데이터를 한번 불러옴
-  // useEffect(() => {
-  //   loadMoreQuestions();
-  // }, [subjectId]);
-
-  // inView가 true 일 때만 데이터를 불러옴!
-  // 보였다 안보이면 true에서 false로 바뀌기 때문에 useEffect가 두번 실행됨!
+  // 스크롤 시 추가 데이터 로딩
+  // if(results.length < limit) limit = results.length // 저는 가보겠습니다!! 나중에 좀 더 코드 정리를.. 하시죠..!!
   useEffect(() => {
-    if (inView) {
-      loadMoreQuestions();
-    }
-  }, [inView, subjectId]);
+    limit = 8;
+    loadMoreQuestions();
+  }, [inView]);
 
   return (
     <Wrapper>
@@ -96,13 +109,8 @@ export default function QuestionFeedPage() {
                 setSubjectId={setSubjectId}
               />
             ))}
-          </FeedBox>
-          <div>
-            {questions.map(data => (
-              <div key={data.subjectId}>{data.content}</div>
-            ))}
             <div ref={ref}>하이</div>
-          </div>
+          </FeedBox>
         </FeedContainer>
       )}
       <QuestionWriteButton
