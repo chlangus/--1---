8b42c1df9547f9b370a3ac060eee12272499742a
Yoojungchoii <geniusyj@gmail.com>
2024-01-29 14:20:41 +0900

    ✨ : 예진님 무한스크롤 구현 QuestionFeedPage에도 적용

--- src/pages/QuestionFeedPage.jsx
+++ src/pages/QuestionFeedPage.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useState, useEffect, useRef } from 'react';
+// import { useInView } from 'react-intersection-observer';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import QuestionFeedHeader from '../components/QuestionFeedHeader/QuestionFeedHeader';
@@ -16,16 +16,16 @@ export default function QuestionFeedPage() {
   const [subjectId, setSubjectId] = useState(id);
   const [questions, setQuestions] = useState([]);
   const [subjectData, setSubjectData] = useSubjectDataRecoil();
-  const [ref, inView] = useInView({
-    triggerOnce: false,
-    rootMargin: '100px',
-  });
-  const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(false);
-  let limit = 8;
+  const [offset, setOffset] = useState(0); // 스크롤이 닿았을 때 새롭게 offset을 바꿈
+  const [loading, setLoading] = useState(false); // 로딩 성공, 실패를 담음
+  const pageEnd = useRef();
 
-  useEffect(() => {
-    fetchQuestion(subjectId).then(data => {
+  const loadMore = () => {
+    setOffset(prev => prev + 5); // 일단 한번에 5개씩 로드하도록 임의로 만들었어용...
+  };
+
+  const fetchPins = async (_id, _offset) => {
+    fetchQuestion(_id, _offset, subjectId).then(data => {
       if (data.results.length) {
         // 데이터 있으면 실행
         const transformedQuestions = data.results.map(question => ({
@@ -39,59 +39,31 @@ export default function QuestionFeedPage() {
               }
             : null,
         }));
-        setQuestions(transformedQuestions);
-        console.log(transformedQuestions);
-      } else {
-        setQuestions([]);
+        setQuestions(prev => [...prev, ...transformedQuestions]);
       }
-    });
-  }, []);
-
-  // ============무한스크롤 구현 시도중====================================
-  async function getQuestions() {
-    const query = `limit=${limit}&offset=${offset}`; //
-    console.log(query);
-    const response = await fetch(
-      `https://openmind-api.vercel.app/3-2/subjects/${subjectId}/questions/?${query}`,
-    );
-    const result = await response.json();
-    if (result.results.length < limit) limit = result.results.length;
-    console.log(result);
-    return result;
-  }
-  // 안녕하십니까!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 어 그런가요? 16개만 나오는듯..!?
-  // 오 어떤가요 먼가 달라졌죠
-  const loadMoreQuestions = async () => {
-    // 좋은 접근입니다!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    if (loading) return; // 이미 로딩 중이면 중복 호출 방지
-    setLoading(true);
-
-    try {
-      const data = await getQuestions();
-      const transformedQuestions = data.results.map(question => ({
-        ...question,
-      }));
-      console.log('transformedData: ', transformedQuestions);
-      if (data.results.length) {
-        // 만약 결과가 있다면 상태를 업데이트 ㅎ 됐당!!!!!!!!!!!!!!!!!!
-        setOffset(prevOffset => prevOffset + limit);
-        setQuestions(prevData => [...prevData, ...transformedQuestions]);
-      }
-      if (loading) return; // 이미 로딩 중이면 중복 호출 방지
       setLoading(true);
-    } catch (error) {
-      console.error('데이터를 가져오는 중 오류 발생:', error);
-    } finally {
-      setLoading(false);
-    }
+    });
   };
-  // 라이브러리를 안 쓰는 것이 덜 복잡할 수도 있습니다!
 
-  // 스크롤 시 추가 데이터 로딩
-  // if(results.length < limit) limit = results.length // 저는 가보겠습니다!! 나중에 좀 더 코드 정리를.. 하시죠..!!
   useEffect(() => {
-    loadMoreQuestions();
-  }, [inView]);
+    fetchPins(subjectId, offset);
+  }, [offset]);
+
+  useEffect(() => {
+    if (loading) {
+      // 로딩되었을 때만 실행
+      const observer = new IntersectionObserver(
+        entries => {
+          if (entries[0].isIntersecting) {
+            loadMore();
+          }
+        },
+        { threshold: 1 },
+      );
+      // 옵저버 탐색 시작
+      if (pageEnd.current) observer.observe(pageEnd.current);
+    }
+  }, [loading]);
 
   return (
     <Wrapper>
@@ -113,6 +85,7 @@ export default function QuestionFeedPage() {
                 setSubjectId={setSubjectId}
               />
             ))}
+            <Loading ref={pageEnd} />
           </FeedBox>
         </FeedContainer>
       )}
@@ -120,10 +93,13 @@ export default function QuestionFeedPage() {
         subjectId={subjectId}
         handleQuestion={setQuestions}
       />
-      <div ref={ref}>하이</div>
     </Wrapper>
   );
 }
+
+const Loading = styled.div`
+  display: flex;
+`;
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.colorGrayScale20};
