047b116b329a12c5e3c3793fa186fd2eb9b55de2
괴도소연\박소연 <rnlduq157@naver.com>
2024-02-03 16:23:39 +0900

    🔥: console.log 제거 ~

--- src/components/List/Pagination/Pagination.jsx
+++ src/components/List/Pagination/Pagination.jsx
@@ -44,9 +44,6 @@ export default function Pagination({
 
   // 페이지 설정
   const handlePageChange = newPage => {
-    console.log('newpage:', newPage);
-    console.log('totalPages:', totalPages);
-    console.log('current:', currentPage);
     if (newPage >= FIRST_PAGE && newPage <= totalPages) {
       setPage(newPage);
       setPageGroups(
@@ -56,45 +53,56 @@ export default function Pagination({
   };
   return (
     <PageButtonWrap>
-      {currentPage > FIRST_PAGE && currentPage !== 2 && (
-        <PageButton type="button" onClick={() => handlePageChange(FIRST_PAGE)}>
-          {'<<'}
-        </PageButton>
-      )}
-      {currentPage > FIRST_PAGE && (
-        <PageButton
-          type="button"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          {'<'}
-        </PageButton>
-      )}
-
-      {pageGroups?.map(page => (
-        <PageButton
-          key={page}
-          type="button"
-          onClick={() => handlePageChange(page)}
-          className={currentPage === page ? 'active' : ''}
-        >
-          {page}
-        </PageButton>
-      ))}
-      {currentPage < totalPages && (
-        <PageButton
-          type="button"
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          {'>'}
-        </PageButton>
-      )}
-      {currentPage < totalPages && currentPage !== totalPages - 1 && (
-        <PageButton type="button" onClick={() => handlePageChange(totalPages)}>
-          {'>>'}
-        </PageButton>
-      )}
+      <PreButtons>
+        {currentPage > FIRST_PAGE && currentPage !== 2 && (
+          <PageButton
+            type="button"
+            onClick={() => handlePageChange(FIRST_PAGE)}
+          >
+            {'<<'}
+          </PageButton>
+        )}
+        {currentPage > FIRST_PAGE && (
+          <PageButton
+            type="button"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            {'<'}
+          </PageButton>
+        )}
+      </PreButtons>
+      <PageNumbers>
+        {pageGroups?.map(page => (
+          <PageButton
+            key={page}
+            type="button"
+            onClick={() => handlePageChange(page)}
+            className={currentPage === page ? 'active' : ''}
+          >
+            {page}
+          </PageButton>
+        ))}
+      </PageNumbers>
+      <AfterButtons>
+        {currentPage < totalPages && (
+          <PageButton
+            type="button"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            {'>'}
+          </PageButton>
+        )}
+        {currentPage < totalPages && currentPage !== totalPages - 1 && (
+          <PageButton
+            type="button"
+            onClick={() => handlePageChange(totalPages)}
+          >
+            {'>>'}
+          </PageButton>
+        )}
+      </AfterButtons>
     </PageButtonWrap>
   );
 }
@@ -105,6 +113,21 @@ const PageButtonWrap = styled.div`
   width: 50rem;
   height: 4rem;
   gap: 1.8rem;
+  position: relative;
+`;
+
+const PageNumbers = styled.div`
+  position: fixed;
+  margin-left: 0.5rem;
+`;
+
+const PreButtons = styled.div`
+  position: absolute;
+  margin-left: 0.5rem;
+`;
+const AfterButtons = styled.div`
+  position: absolute;
+  margin-left: 0.5rem;
 `;
 
 const PageButton = styled.button`
